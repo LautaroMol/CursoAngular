71024d1cd38654e766bbe57214cdd5a97172a36e
Lautaro <laumol159@gmail.com>
2023-02-14 19:52:13 -0300

    V2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 interface Tarjeta {
   titulo: string;
   subtitulo: string;
-  nro?:number;
+  image:string;
 }
 
 @Component({
@@ -12,15 +12,21 @@ interface Tarjeta {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  nombre = 'nombre'; 
   title = 'my-dream-app';
   public ArregloTarjetas: Tarjeta[] = [];
+  textPlaceHolder = "escriba su nombre";
+  deshabilitado = true;
   
   ngOnInit():void {
-
     this.ArregloTarjetas = [
-      {titulo:"video1",subtitulo:"subtitulo 1",nro:1},
-      {titulo:"video2",subtitulo:"subtitulo 2"},
-      {titulo:"video3",subtitulo:"subtitulo 3"},
+      {titulo:"video1",subtitulo:"subtitulo 1",image:"https://steamuserimages-a.akamaihd.net/ugc/2024961644120991619/420213DC1E3B88834BCB2A1028C8C72406FE459D/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"},
+      {titulo:"video2",subtitulo:"subtitulo 2",image: "https://steamuserimages-a.akamaihd.net/ugc/2024961644120991619/420213DC1E3B88834BCB2A1028C8C72406FE459D/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"},
+      {titulo:"video3",subtitulo:"subtitulo 3",image:"https://steamuserimages-a.akamaihd.net/ugc/2024961644120991619/420213DC1E3B88834BCB2A1028C8C72406FE459D/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"},
     ]
+  }
+  constructor() {
+    setInterval(()=>this.nombre='nombre 2',3000) ///cambia la variable nombre a nombre 2 despues de 3 segundos
+    setInterval(()=>this.deshabilitado=false,3000);
   }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class CardComponent {
   public image2:string = 'http://photoshop-kopona.com/uploads/posts/2019-05/1559108923_0-2.jpg';
   public Titulo:string = "curso de angular con interpolacion"
+  image:string=""
+  @Input() dataentrante:any;
   constructor(){}
   
   ngOnInit(): void{}
