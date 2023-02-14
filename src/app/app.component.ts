8b3fc53468c3c2f5bb1288f04f78be097ef5eb51
Lautaro <laumol159@gmail.com>
2023-02-14 16:26:39 -0300

    V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Tarjeta {
+  titulo: string;
+  subtitulo: string;
+  nro?:number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  public ArregloTarjetas: Tarjeta[] = [];
+  
+  ngOnInit():void {
+
+    this.ArregloTarjetas = [
+      {titulo:"video1",subtitulo:"subtitulo 1",nro:1},
+      {titulo:"video2",subtitulo:"subtitulo 2"},
+      {titulo:"video3",subtitulo:"subtitulo 3"},
+    ]
+  }
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  public image2:string = 'http://photoshop-kopona.com/uploads/posts/2019-05/1559108923_0-2.jpg';
+  public Titulo:string = "curso de angular con interpolacion"
+  constructor(){}
+  
+  ngOnInit(): void{}
+}
